665b16aa73255a14aa685453a3b8e4e57e9228ef
Webonaute <mdelisle@webonaute.ca>
2014-06-28 10:55:44 -0400

    Add missing docblock

--- Tests/Service/NonSenseServiceTest.php
+++ Tests/Service/NonSenseServiceTest.php
@@ -1,12 +1,27 @@
 <?php
-
+/*
+* This file is part of the FOSUserBundle package.
+*
+* (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
 namespace FOS\UserBundle\Tests\Security;
 
 use Webonaute\NonSenseBundle\Service\NonSenseService;
 
+/**
+ * Class NonSenseServiceTest
+ *
+ * @package FOS\UserBundle\Tests\Security
+ */
 class NonSenseServiceTest extends \PHPUnit_Framework_TestCase
 {
 
+    /**
+     * Test the service return a not empty string.
+     */
     public function testRefreshInvalidUserClass()
     {
         $nonSenseService = new NonSenseService();
